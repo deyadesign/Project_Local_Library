45d05ec825ea4fee267af45523face4186a51acf
Deyanira Davila <112524361+deyadesign@users.noreply.github.com>
2023-01-10 18:37:44 -0600

    Test function
    Added to test function to include a test function.

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -2,6 +2,13 @@ function findAccountById(accounts, id) {
   let foundId = accounts.find((account) => account.id === id);
    return foundId;
 }
+
+// test function/
+if (typeof findAccountById === "function") {
+  console.log("Test passed.It is a function");
+} else {
+  console.log("findAccountById() is not a function.");
+}
   
 
 function sortAccountsByLastName(accounts) {
